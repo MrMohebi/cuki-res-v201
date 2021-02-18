9d6db9de8aa237952b0d524e686a61d3e5cba595
MrM <MMMohebi@outlook.com>
2021-02-18 16:26:49 +0330

    init

---
+++ src/Stores/reduxStore/reducers/reducerRestaurantUser.js
@@ -0,0 +1,24 @@
+import {__init__RestaurantUser} from "./__init__Reducers";
+import * as actionTypes from '../actionTypes'
+import produce from "immer";
+
+
+
+export default function reducerRestaurantUser (state=__init__RestaurantUser, action){
+    switch (action.type) {
+        case actionTypes.SET_USER_INFO :
+            return(produce(state, stateDraft=>{
+                stateDraft.token = action.payload.token;
+                stateDraft.position = action.payload.position;
+                stateDraft.username = action.payload.username;
+                stateDraft.englishName = action.payload.englishName;
+                stateDraft.persianName = action.payload.persianName;
+            }))
+        case actionTypes.SET_TOKEN :
+            return(produce(state, stateDraft=>{
+                stateDraft.token = action.payload.token;
+            }))
+        default:
+            return state;
+    }
+}
