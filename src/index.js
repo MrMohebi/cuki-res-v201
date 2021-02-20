aa0442e1ed3b52277f707b3a7985683e35f836a0
Mokafela <mkd0098@gmail.com>
2021-02-20 11:42:08 +0330

    Store new location included

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {store} from "./reduxStore/store";
+import {store} from "./Stores/reduxStore/store";
 import {Provider} from "react-redux";
 ReactDOM.render(
     <Provider store={store}>
