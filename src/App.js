7950f6b10c28b5fb4b13a8c54da234624e622758
Mokafela <mkd0098@gmail.com>
2021-02-20 11:38:14 +0330

    new App js initialized (unusable routes Removed)

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import '@popperjs/core/dist/cjs/popper'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle'
 import Signin from "./Components/signin/Signin";
-import Sidebar from "./Components/sidebar/Sidebar";
 import Dashboard from "./Components/dashboard/Dashboard";
 import Foods from "./Components/foods/Foods";
 import EachFood from "./Components/eachFood/EachFood";
-import RestaurantInfo from "./Components/restaurantInfo/RestaurantInfo";
-
-import Table from "./Components/table/Table";
-import SelectHours from "./Components/restaurantInfo/js/selectHours";
 import Orders from "./Components/orders/Orders";
 
 $("body").css({backgroundColor: '#b7b1c2'})
@@ -21,17 +16,11 @@ function App() {
   return (
     <div>
       <Router>
-          <Route  path='/*' render={ ( props ) => ( props.location.pathname !== "/" && props.location.pathname !== "/signin") && <Sidebar /> }/>
           <Route exact path='/' component={Signin}/>
           <Route exact path='/dashboard' component={Dashboard}/>
           <Route exact path='/foods' component={Foods}/>
-          <Route path='/foods/:foodId' component={EachFood}/>
-          <Route path='/resInfo' component={RestaurantInfo}/>
-          <Route path='/sh' component={SelectHours}/>
-          <Route path='/ct' component={Table}/>
+          <Route path='/foodInfo' component={EachFood}/>
           <Route path='/orders' component={Orders}/>
-
-
       </Router>
 
     </div>
