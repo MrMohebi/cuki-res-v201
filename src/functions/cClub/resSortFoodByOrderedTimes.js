423d98030ced16ceae5d6785f6dd9fb25d6b6908
MrMohebi <MMMohebi@outlook.com>
2021-04-17 14:34:14 +0430

    change name

--- src/functions/cClub/resSortFoodByOrderedTimes.js
+++ src/functions/cClub/resSortFoodByOrderedTimes.js
@@ -1,3 +1,3 @@
-function sortFoodByOrderedTimes(foodList){
+function resSortFoodByOrderedTimes(foodList){
     return foodList.sort(function (a, b) {return parseInt(a.order_times) - parseInt(b.order_times)}).reverse()
 }
